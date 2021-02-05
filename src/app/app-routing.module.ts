efc82caa64f33afb6893043542dbed6796c8f9c5
Axel Sparkster <axelsparkster@gmail.com>
2021-02-05 14:55:35 -0600

    feat: add routing to multiple pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component'
+import { Page1Component } from './page1/page1.component'
+import { Page2Component } from './page2/page2.component'
+
 const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
     component: HomeComponent
-  }
+  },
+  {
+    path: 'page1',
+    component: Page1Component
+  },
+  {
+    path: 'page2',
+    component: Page2Component
+  },
+
+  // just redirect home if it's not found
+  {
+    path: '**',
+    redirectTo: ''
+  },
 ];
 
 @NgModule({
