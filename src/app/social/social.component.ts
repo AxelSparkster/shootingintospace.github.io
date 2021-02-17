2bcc819fa1e9416be10f75e77ca20ac5b20c1b02
Axel Sparkster <axelsparkster@gmail.com>
2021-02-17 01:04:42 -0600

    feat: add container for social component

--- src/app/social/social.component.ts
+++ src/app/social/social.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { fadeInDownAnimation, fadeInDownOnEnterAnimation } from 'angular-animations';
 
 @Component({
   selector: 'app-social',
   templateUrl: './social.component.html',
-  styleUrls: ['./social.component.scss']
+  styleUrls: ['./social.component.scss'],
+  animations: [
+    fadeInDownOnEnterAnimation({ anchor: 'enter', duration: 1000})
+  ]
 })
 export class SocialComponent implements OnInit {
 
